f0d70ebabbfff252faf38ca9b9ffa6e3492a62c6
Kshitij Gupta <kshitijqwerty@gmail.com>
2021-11-22 12:37:09 +0530

    added more utilities

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ def getSHA(path):
         hash = hashlib.sha256(data).hexdigest()
         return hash
 
-def compress(path):
+def compress_file(path):
     data = open(path, 'rb').read()
     hash = hashlib.sha256(data).hexdigest()
     c_data = zlib.compress(data, zlib.Z_BEST_COMPRESSION)
@@ -24,31 +24,41 @@ def compress(path):
     dic['sha256'] = hash
     return dic
 
-def compress(dic):
+def compress_tree(dic):
     data = pickle.dumps(dic)
     hash = hashlib.sha256(data).hexdigest()
     c_data = zlib.compress(data, zlib.Z_BEST_COMPRESSION)
     f = open('./.vcs/objs/'+hash, 'wb')
     f.write(c_data)
     f.close()
-    perm = oct(os.stat('./.vcs/objs/'+hash).st_mode)[2:]
-    fname = os.path.basename('./.vcs/objs/'+hash)
+    perm = oct(os.stat('./.vcs/obj/'+hash).st_mode)[2:]
+    fname = os.path.basename('./.vcs/obj/'+hash)
     dic = {}
     dic['filename'] = fname
     dic['permissions'] = perm
     dic['sha256'] = hash
     return dic
 
-def decompress(hash,path):
-    data = open('./git/objs/'+hash, 'rb').read()
+def decompress_file(hash,path):
+    data = open('./.vcs/obj/'+hash, 'rb').read()
     data = zlib.decompress(data)
     f = open(path, 'wb')
     f.write(data)
     f.close()
 
-def decompress(hash):
-    data = open('./git/objs/'+hash, 'rb').read()
+def decompress_tree(hash):
+    data = open('./.vcs/obj/'+hash, 'rb').read()
     data = zlib.decompress(data)
     dic = pickle.loads(data)
     return dic
+     
 def get_head():
+    data = open('./.vcs/HEAD', 'rb').read()
+    return data
+
+def get_branch(name):
+    data = open('./.vcs/branch/'+name).read()
+    return data
+
+
+
